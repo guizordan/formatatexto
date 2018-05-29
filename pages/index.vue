a693cb4444a9e65ec46e6a062d5afc468ee7d88b
Guilherme Zordan <guilhermezordan@outlook.com>
2018-05-29 18:07:38 -0300

    method that remove extra whitespaces

--- pages/index.vue
+++ pages/index.vue
@@ -10,46 +10,54 @@
 
         <button @click="upcase" class="btn">Capitalizar</button>
         <button @click="downcase" class="btn ml-2">Minimizar</button>
+        <button @click="remove_spaces" class="btn ml-2">Remove Espaços</button>
     </div>
 
   </div>
 
-    <div class="columns">
+  <div class="columns">
     <div class="column col-6 col-ml-auto col-mr-auto">
         <div class="form-group">
-            <textarea v-model="output" class="form-input" rows="5"></textarea>
+          <textarea v-model="output" class="form-input" rows="5"></textarea>
         </div>
     </div>
-
   </div>
 
 </div>
 
-
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+// import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-    data(){
-        return{
-            input: null,
-            output: null
+  data(){
+      return{
+        input: null,
+        output: null
+      }
+  },
+
+  methods: {
+      upcase: function(){
+        this.output = this.input.toUpperCase();
+      },
+
+      downcase: function(){
+        this.output = this.input.toLowerCase();
+      },
+
+      remove_spaces: function(){
+        this.output = this.input;
+        for (let i = 0; i <= this.output.length; i++){
+          console.log(this.output[i]);
+          if (this.output[i].match(/\s/) && this.output[i+1].match(/\s/)){
+            console.log(this.output[i], 'matches');
+            this.output[i+1] = '';
+          }
         }
-    },
-
-    methods: {
-        upcase: function(){
-            console.log('method');
-            this.output = this.input.toUpperCase();
-        },
-
-        downcase: function(){
-            console.log('method');
-            this.output = this.input.toLowerCase();
-        }
-    }
-
+      }
+      
+  }
 }
 </script>
