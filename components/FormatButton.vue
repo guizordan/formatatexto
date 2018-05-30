9c1cc464c6f62419dc48fae7fe69ed4eb25f6873
Guilherme Zordan <guilhermezordan@outlook.com>
2018-05-30 13:18:13 -0300

    format button isolated in one component

---
+++ components/FormatButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button ref="button" @click="activate" class="btn">{{label}}</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: false
+    };
+  },
+
+  props: ['label'],
+
+  methods: {
+    activate: function() {
+      if (this.active) {
+        this.$refs.button.classList.remove("active");
+        this.$emit("deactivate", input);
+        this.active = false;
+      } else {
+        this.$refs.button.classList.add("active");
+        this.$emit("activate");
+        this.active = true;
+      }
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,81 @@
 <template>
 
 <div class="container mt-2">
-  <div class="columns mb-2">
-    <div class="column col-6 col-ml-auto col-mr-auto">
-        <h1>Formatar Texto</h1>
-        <div class="form-group">
-            <textarea v-model="input" class="form-input" rows="5"></textarea>
-        </div>
-
-        <button @click="upcase" class="btn">Capitalizar</button>
-        <button @click="downcase" class="btn ml-2">Minimizar</button>
-        <button @click="remove_spaces" class="btn ml-2">Remove Espaços</button>
-    </div>
-
-  </div>
-
   <div class="columns">
-    <div class="column col-6 col-ml-auto col-mr-auto">
-        <div class="form-group">
-          <textarea v-model="output" class="form-input" rows="5"></textarea>
+    <div class="column col-sm-12 col-6 col-ml-auto col-mr-auto">
+        
+      <div class="columns">
+
+        <div class="column col-12 mb-2">
+          <h1>Formatar Texto</h1>
+          <div class="form-group">
+              <textarea v-model="input" class="form-input" rows="5"></textarea>
+          </div>
         </div>
+
+        <div class="column col-12 mb-2">
+          <button ref="upcase" @click="upcase" class="btn">Capitalizar</button>
+          <button ref="downcase" @click="downcase" class="btn ml-2">Minimizar</button>
+          <button ref="remove_whitespaces" @click="remove_whitespaces" class="btn ml-2">Remove Espaços</button>
+          <format-button label="botao"></format-button>
+        </div>
+
+
+        <div class="column col-12">
+            <div class="form-group">
+              <textarea v-model="output" class="form-input" rows="5"></textarea>
+            </div>
+        </div>
+
+      </div>
     </div>
+
   </div>
+
 
 </div>
 
 </template>
 
 <script>
-// import AppLogo from '~/components/AppLogo.vue'
+import FormatButton from "~/components/FormatButton.vue"
 
 export default {
-  data(){
-      return{
-        input: null,
-        output: null
-      }
+  data() {
+    return {
+      input: null,
+      output: null
+    };
+  },
+
+  components: {
+    FormatButton
+  },
+
+  mounted() {
+    console.log(this.$refs);
   },
 
   methods: {
-      upcase: function(){
-        this.output = this.input.toUpperCase();
-      },
+    upcase: function() {
+      this.output = this.input.toUpperCase();
+    },
 
-      downcase: function(){
-        this.output = this.input.toLowerCase();
-      },
+    downcase: function() {
+      this.output = this.input.toLowerCase();
+    },
 
-      remove_spaces: function(){
-        this.output = this.input;
-        for (let i = 0; i <= this.output.length; i++){
-          console.log(this.output[i]);
-          if (this.output[i].match(/\s/) && this.output[i+1].match(/\s/)){
-            console.log(this.output[i], 'matches');
-            this.output[i+1] = '';
-          }
-        }
+    remove_whitespaces: function() {
+      var output = [];
+      for (let i = 0; i < this.input.length; i++) {
+        if (this.input[i].match(/\s/) && this.input[i + 1].match(/\s/))
+          continue;
+        else output[i] = this.input[i];
       }
-      
+
+      this.output = output.join("");
+      this.$refs.remove_whitespaces.classList.add("active");
+    }
   }
-}
+};
 </script>
